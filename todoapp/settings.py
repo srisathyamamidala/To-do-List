e26cfded6dee7bdc26abe27d0533885e4fd09bb1
srisathyamamidala <77813332+srisathyamamidala@users.noreply.github.com>
2022-03-21 09:48:15 -0500

    database connected and the image added

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -76,14 +76,19 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'todo',
-        'USER':'postgres',
-        'PASSWORD':'1234',
-        'HOST':'localhost',
-        'PORT': 5433
+   # 'default': {
+   #     'ENGINE': 'django.db.backends.postgresql',
+    #    'NAME': 'todo',
+    #    'USER':'postgres',
+    #    'PASSWORD':'1234',
+     #   'HOST':'localhost',
+    #    'PORT': 5433
+   # }
+   'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR + '/db.sqlite3',
     }
+
 }
 
 
